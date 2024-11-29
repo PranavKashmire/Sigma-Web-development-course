2b5faf46deabb294331a1ae2ab9aca741c6f07fe
Pranav Kashmire <pranav.kashmire09@gmail.com>
2024-11-29 13:35:20 +0530

    Add files via upload

---
+++ 61.solution 9/solution9.js
@@ -0,0 +1,33 @@
+// note: 1. prompt is used to the take input from user in JS
+// 2. see for the function eval in JS
+// 3. this question can be solved using a ifelse ladder , but we will do it using the eval function
+// 4. console.log is used to print
+
+let random = Math.random()
+console.log(random)
+let a = prompt("Enter first number")
+let c = prompt("Enter operation")
+let b = prompt("Enter second number")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+
+
+if (random > 0.1) {
+    // Perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    
+}
+
+else {
+    // Perform wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+
+}
