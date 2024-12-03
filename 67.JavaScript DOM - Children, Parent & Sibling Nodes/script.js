1ba70c79707ec9b38a8c101aef84bea60ec123ce
Pranav Kashmire <pranav.kashmire09@gmail.com>
2024-12-03 12:02:42 +0530

    Add files via upload

---
+++ 67.JavaScript DOM - Children, Parent & Sibling Nodes/script.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+
+// document.body.firstElementChild.children
+// we get all the element nodes
+
+// document.body.firstElementChild.children[3]
+// is used to get the next sibling
+
+
+// to get the previous element sibling we use :
+// document.body.firstElementChild.previousSibling
