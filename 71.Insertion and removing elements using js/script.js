948ea4d47a825a68f08d48da45b07a334557c3e4
Pranav Kashmire <pranav.kashmire09@gmail.com>
2024-12-07 16:11:36 +0530

    Add files via upload

---
+++ 71.Insertion and removing elements using js/script.js
@@ -0,0 +1,6 @@
+// doing this in the console menu we can get the html code inside that element
+// document.querySelector(".box").innerHTML
+
+// shows the inner html as text 
+// document.querySelector(".contianer").innerText
+
